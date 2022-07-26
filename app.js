534d9e645c13e9bac42625c92f826f7fe96203f1
haru864 <kawachigenji95@gmail.com>
2022-07-27 02:19:58 +0900

    app.js, add function to add status to header

--- app.js
+++ app.js
@@ -6,16 +6,38 @@ const allSections = document.querySelectorAll('.main-content');
 function PageTransitions() {
     // Button click active class
     for (let i = 0; i < sectBtn.length; i++) {
+        // console.log(sectBtn[i]);
         sectBtn[i].addEventListener('click', function () {
             let currentBtn = document.querySelectorAll('.active-btn');
-            // console.log(currentBtn);
-            // console.log(this.classList);
             for (let j = 0; j < currentBtn.length; j++) {
                 currentBtn[0].classList.remove('active-btn');
             }
             this.classList.add('active-btn');
-        })
+        });
     }
+
+    // Sections Active
+    allSections.forEach((section) => {
+        // console.log(section);
+        section.addEventListener('click', (e) => {
+            const id = e.target.dataset.id;
+            if (id) {
+                // remove selected from the other btns
+                sectBtns.forEach((btn) => {
+                    btn.classList.remove('active');
+                });
+                e.target.classList.add('active');
+
+                // hide other sections
+                sections.forEach((section) => {
+                    section.classList.remove('active');
+                });
+
+                const element = document.getElementById(id);
+                element.classList.add('active');
+            }
+        })
+    });
 }
 
 PageTransitions();
